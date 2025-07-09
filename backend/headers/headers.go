71f221badd20694d5720eabead057f2b2d53d2b2
Stephanny <kauanestephannny@gmail.com>
2025-07-09 16:54:19 -0300

    env access control origin backend

--- backend/headers/headers.go
+++ backend/headers/headers.go
@@ -2,24 +2,27 @@ package headers
 
 import (
 	"net/http"
-    
+	"os"
 )
 
 func SetHeaders(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    
-        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH")
-        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-        w.Header().Set("Access-Control-Allow-Credentials", "true")
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-    
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
+		origin := os.Getenv("FRONTEND_ORIGIN")
+		if origin == "" {
+			origin = "*"
+		}
+
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
-        next.ServeHTTP(w, r)
-    })
+		next.ServeHTTP(w, r)
+	})
 }
-
