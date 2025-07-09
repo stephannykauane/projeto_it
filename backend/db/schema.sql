ea51d622bc80d63789b0d72a7f2a5ba852840319
Stephanny <kauanestephannny@gmail.com>
2025-07-09 16:11:03 -0300

    read to deploy

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,29 +1,56 @@
 CREATE TABLE Usuario (
-    id_usuario SERIAL PRIMARY KEY,
-    email VARCHAR NOT NULL,
-    senha VARCHAR NOT NULL,
-    nome VARCHAR
+  id SERIAL PRIMARY KEY,
+  senha VARCHAR,
+  nome VARCHAR,
+  email VARCHAR UNIQUE
 );
 
-CREATE TABLE Metodo (
-    id_metodo SERIAL PRIMARY KEY,
-    nome_metodo VARCHAR NOT NULL
-);
 
-CREATE TABLE Calculo (
-    id_calculo SERIAL PRIMARY KEY,
-    resultado NUMERIC,
-    data_calculo DATE
+CREATE TABLE Area (
+  id SERIAL PRIMARY KEY,
+  consultor VARCHAR,
+  propriedade VARCHAR,
+  area VARCHAR,
+  id_usuario INTEGER
 );
 
 CREATE TABLE Analise (
-    id_analise SERIAL PRIMARY KEY,
-    hidrogenio NUMERIC,
-    potassio NUMERIC,
-    magnesio NUMERIC,
-    aluminio NUMERIC,
-    calcio NUMERIC,
-    sat_desejada NUMERIC,
-    prnt NUMERIC,
-    ctc NUMERIC
+  id SERIAL PRIMARY KEY,
+  id_usuario INTEGER,
+  magnesio NUMERIC,
+  aluminio NUMERIC,
+  calcio NUMERIC,
+  sat_desejada NUMERIC,
+  prnt NUMERIC,
+  ctc NUMERIC,
+  argila NUMERIC,
+  sat_atual NUMERIC,
+  teor_ca NUMERIC,
+  caO NUMERIC,
+  mgO NUMERIC,
+  teor_mg NUMERIC,
+  mg_desejada NUMERIC,
+  ca_desejada NUMERIC,
+  id_area INTEGER
 );
+
+
+CREATE TABLE Calculo (
+  id SERIAL PRIMARY KEY,
+  id_analise INTEGER,
+  resultado NUMERIC,
+  data_calculo DATE,
+  id_metodo INTEGER
+);
+
+ALTER TABLE Area
+  ADD CONSTRAINT Area_id_usuario_fkey FOREIGN KEY (id_usuario) REFERENCES Usuario(id);
+
+ALTER TABLE Analise
+  ADD CONSTRAINT Analise_id_usuario_fkey FOREIGN KEY (id_usuario) REFERENCES Usuario(id);
+
+ALTER TABLE Analise
+  ADD CONSTRAINT fk_analise_area FOREIGN KEY (id_area) REFERENCES Area(id);
+
+ALTER TABLE Calculo
+  ADD CONSTRAINT Calculo_id_analise_fkey FOREIGN KEY (id_analise) REFERENCES Analise(id);
