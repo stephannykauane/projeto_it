a4602751b5b5fbfc638a7cc0f7dae5292a2fe65d
Stephanny <kauanestephannny@gmail.com>
2025-07-09 16:31:49 -0300

    env port backend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/stephannykauane/projeto_it/backend/db"
 	"github.com/stephannykauane/projeto_it/backend/headers"
 	"github.com/stephannykauane/projeto_it/backend/routes"
-	"net/http"
 )
 
 func main() {
-
 	db.Database()
 
 	mux := http.NewServeMux()
-
 	routes.RegisterRoutes(mux)
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Backend funcionando!")
 	})
 
-	fmt.Println("Servidor Go iniciado em http://localhost:8080")
-	
-	err := http.ListenAndServe(":8080", headers.SetHeaders(mux))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Servidor Go iniciado na porta " + port)
+
+	err := http.ListenAndServe(":"+port, headers.SetHeaders(mux))
 	if err != nil {
 		fmt.Println("Erro ao iniciar o servidor:", err)
 	}
-
 }
