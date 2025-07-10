5b089aab647afbbece3c0893fc77ff3d3c2b77cb
Stephanny <kauanestephannny@gmail.com>
2025-07-10 15:11:09 -0300

    fix: cors

--- backend/headers/headers.go
+++ backend/headers/headers.go
@@ -9,17 +9,15 @@ func SetHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		origin := os.Getenv("FRONTEND_ORIGIN")
-		if origin == "" {
-			origin = "*"
-		}
-
+		
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusNoContent)
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
 			return
 		}
 
