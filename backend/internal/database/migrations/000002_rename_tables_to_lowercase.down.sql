d17c27fd3d04724534cf22e9794d73df70f254c3
Stephanny <kauanestephannny@gmail.com>
2025-08-05 19:52:44 -0300

    new migration

--- backend/internal/database/migrations/000002_rename_tables_to_lowercase.down.sql
+++ backend/internal/database/migrations/000002_rename_tables_to_lowercase.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "Usuario" RENAME TO usuario;
 ALTER TABLE "Analise" RENAME TO analise;
 ALTER TABLE "Calculo" RENAME TO calculo;
-ALTER TABLE "Area" RENAME TO area;
+ALTER TABLE "rea" RENAME TO area;
--- backend/internal/database/migrations/000003_rename_tables_to_lowercase.down.sql
+++ backend/internal/database/migrations/000003_rename_tables_to_lowercase.down.sql
@@ -2,4 +2,3 @@ ALTER TABLE usuario RENAME TO "Usuario";
 ALTER TABLE analise RENAME TO "Analise";
 ALTER TABLE calculo RENAME TO "Calculo";
 ALTER TABLE area RENAME TO "Area";
-
