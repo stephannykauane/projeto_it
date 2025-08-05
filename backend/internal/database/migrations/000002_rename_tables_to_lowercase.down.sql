7e85a61b36e593714452bb444b7f8a9db9a57c69
Stephanny <kauanestephannny@gmail.com>
2025-08-05 19:46:29 -0300

    modificando nome das tabelas e adicionando nova migrations

--- backend/internal/database/migrations/000002_rename_tables_to_lowercase.down.sql
+++ backend/internal/database/migrations/000002_rename_tables_to_lowercase.down.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "Usuario" RENAME TO usuario;
 ALTER TABLE "Analise" RENAME TO analise;
 ALTER TABLE "Calculo" RENAME TO calculo;
-ALTER TABLE "area" RENAME TO area;
+ALTER TABLE "Area" RENAME TO area;
